d2555db03bbf39158487e47527144849b265c1b2
JakeAve <jacobjavery@gmail.com>
2020-08-17 20:09:20 -0600

    added some i18n base work

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 const options = {
-    setHeaders: res => {
-        res.set('Service-Worker-Allowed', '/');
-    }
-};
+  setHeaders: (res) => {
+    res.set('Service-Worker-Allowed', '/')
+  },
+}
 
-app.use(express.static('dist', options))
+app.use('/', express.static('dist', options))
+app.use(
+  '/api/lang',
+  express.static('dist/lang', { ...options, extensions: ['json'] })
+)
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000');
+  console.log('Listening on port 3000')
 })
--- webpack.config.js
+++ webpack.config.js
@@ -6,41 +6,47 @@ const copyPlugin = require('copy-webpack-plugin')
 const config = {
   entry: {
     main: './src/js/main.js',
-    styles: './src/scss/styles.scss'
+    styles: './src/scss/styles.scss',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
+    filename: '[name].js',
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: '[name].css'
+      filename: '[name].css',
     }),
     new ExtraneousFileCleanupPlugin({
       extensions: ['.js'],
-      paths: ['/styles']
+      paths: ['/styles'],
     }),
     new copyPlugin([
       {
         from: './src',
         to: '',
-        ignore: ['js/**/*', 'scss/**/*']
-      }
-    ])
+        ignore: ['js/**/*', 'scss/**/*'],
+      },
+    ]),
+    new copyPlugin([
+      {
+        from: './src/lang',
+        to: '/lang',
+      },
+    ]),
   ],
   module: {
     rules: [
       {
         test: /\.js?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.scss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      }
-    ]
-  }
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
 }
 
 module.exports = [config]
